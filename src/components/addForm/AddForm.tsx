c8c4e3401401407c7e7ab172b393884c59820778
MichalPietraszek <michal.pietraszek98@gmail.com>
2022-05-03 11:13:40 +0200

    add exit button

--- src/components/addForm/AddForm.tsx
+++ src/components/addForm/AddForm.tsx
@@ -1,24 +1,32 @@
 import React, {useState} from "react";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../features/store";
 import {StarRating} from "../starRating/StarRating";
 
 import './AddForm.css';
+import {setVisibility} from "../../features/addMovie/addMovie-slice";
 
 export const AddForm = () => {
+    const dispatch = useDispatch()
     const {visible} = useSelector((store: RootState) => store.addMovie)
-
+    const [selected, setSelected] = useState('Select')
     const [rate, setRate] = useState<number>(0)
 
     const onHandleStarRating = (e: number) => {
         setRate(e)
     }
 
+
     return <div className='AddForm' style={{visibility: visible}}>
+        <button className='ExitButton' onClick={() => dispatch(setVisibility('hidden'))} >x</button>
         <form>
             <label>
-                <select name="" id="">
-                    <option value=""></option>
+                Type <br/>
+                <select value={selected} onChange={e => setSelected(e.target.value)}>
+                    <option value="select">select</option>
+                    <option value="drama">Drama</option>
+                    <option value="comedy">comedy</option>
+                    <option value="action">action</option>
                 </select>
             </label>
             <label>
@@ -34,7 +42,7 @@ export const AddForm = () => {
                     onlyRead
                 />
             </label>
-            <button type='submit'>Add</button>
+            <button className='SubmitButton' type='submit'>Add</button>
         </form>
     </div>
 }
