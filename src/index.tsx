76ec981a569c6bf0ee1a87942c6910a9e5a0cf5d
MichalPietraszek <michal.pietraszek98@gmail.com>
2022-05-26 19:05:17 +0200

    Added Login panel

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import  *  as  ReactDOMClient  from  'react-dom/client' ;
+import *  as  ReactDOMClient from 'react-dom/client' ;
 import React from 'react';
-import {App} from "./components/App/App";
+import {Main} from "./components/Main/Main";
 
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
 
 const container = document.getElementById('root')
 const root = ReactDOMClient.createRoot((container as HTMLElement))
-root.render(<BrowserRouter><App/></BrowserRouter>)
+root.render(<BrowserRouter><Main/></BrowserRouter>)
